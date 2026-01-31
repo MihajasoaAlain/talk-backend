446a0231f2df89c2a324db1f5c9a1d277c557059
Mihajasoa <mihajasoaalain85@gmail.com>
2026-01-31 13:00:02 +0300

    implement user model with password hashing and create user repository and service

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,11 +1,26 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	ID        int       `json:"id"`
 	Username  string    `json:"username" binding:"required"`
 	Email     string    `json:"email" binding:"required,email"`
+	Password  string    `json:"password" binding:"required"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+	return nil
 }
---
+++ internal/repository/user_repository.go
@@ -0,0 +1,23 @@
+package repository
+
+import (
+	"talk-backend/internal/models"
+
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	Create(user *models.User) error
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{db: db}
+}
+
+func (r *userRepository) Create(user *models.User) error {
+	return r.db.Create(user).Error
+}
