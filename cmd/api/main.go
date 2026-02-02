8ae3a247b0e991f1569473fd9eff4373c04c7c4e
Mihajasoa <mihajasoaalain85@gmail.com>
2026-02-02 22:18:21 +0300

    refactor main and migration logic; initialize container and register HTTP routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,22 +3,25 @@ package main
 import (
 	"log"
 	"talk-backend/internal/config"
+	"talk-backend/internal/container"
 	"talk-backend/internal/db"
+	"talk-backend/internal/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	cfg := config.Load()
+
 	if cfg.App.Env == "production" {
 		gin.SetMode(gin.ReleaseMode)
 	}
+
 	gdb, err := db.ConnectPostgres(cfg)
-	log.Println(gdb)
 	if err != nil {
 		log.Fatal(err)
 	}
-	r := gin.Default()
+
 	if cfg.Migration.Valided {
 		log.Println("running database migrations...")
 		if err := db.Migrate(gdb); err != nil {
@@ -27,6 +30,13 @@ func main() {
 		log.Println("database migrations completed")
 	}
 
+	app := container.New(cfg, gdb)
+
+	r := gin.Default()
+	http.RegisterRoutes(r, app)
+
 	log.Printf("Starting server on :%s (%s)", cfg.App.Port, cfg.App.Env)
-	r.Run(":" + cfg.App.Port)
+	if err := r.Run(":" + cfg.App.Port); err != nil {
+		log.Fatalf("server failed: %v", err)
+	}
 }
