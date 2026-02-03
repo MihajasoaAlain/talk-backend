9d7f0e656a5ebb9df50f15fb4b6ea019e3e11f89
Mihajasoa <mihajasoaalain85@gmail.com>
2026-02-03 21:36:26 +0300

    add FindByID method to UserRepository for user retrieval by ID

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -11,6 +11,7 @@ var ErrUserNotFound = errors.New("user not found")
 
 type UserRepository interface {
 	Create(user *models.User) error
+	FindByID(id uint) (*models.User, error)
 	FindByEmail(email string) (*models.User, error)
 	Update(user *models.User) error
 }
@@ -21,6 +22,18 @@ func NewUserRepository(db *gorm.DB) UserRepository { return &userRepository{db: 
 
 func (r *userRepository) Create(user *models.User) error {
 	return r.db.Create(user).Error
+}
+
+func (r *userRepository) FindByID(id uint) (*models.User, error) {
+	var user models.User
+	err := r.db.First(&user, id).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, ErrUserNotFound
+		}
+		return nil, err
+	}
+	return &user, nil
 }
 
 func (r *userRepository) FindByEmail(email string) (*models.User, error) {
